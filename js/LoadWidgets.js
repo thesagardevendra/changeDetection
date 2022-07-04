76d7dd11d4a25688f5ae4956b9022f8fc8a963bc
unknown <aioninfosystems01@gmail.com>
2022-07-04 16:48:55 +0530

    first commit

--- js/LoadWidgets.js
+++ js/LoadWidgets.js
@@ -7,13 +7,13 @@ require([
     "js/DistanceMeasure.js",
     "js/BaseMap.js",
     "js/SlideWidget.js",
-    "js/Screenshot.js",
+    "js/ScreenShot.js",
     "js/Direction.js",
     "js/DayLight.js",
     "js/LineOfSight.js",
     "js/Selection.js",
     "js/SearchWidget.js",
-    "js/Charts.js",
+    "js/charts.js",
     "js/DataTable.js"
 ], function() { 
     TimeSlider.init();
